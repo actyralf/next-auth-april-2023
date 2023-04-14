708045b2e439849168ca2103b50dce8b12dd7c82
Ralf Siewert <ralf.siewert@actyvyst.com>
2023-04-14 15:21:55 +0200

    add auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import GlobalStyles from "../styles/GlobalStyles";
 import {StyledContainer} from "../components/StyledContainer";
+import {SessionProvider} from "next-auth/react";
 
-function MyApp({Component, pageProps: {...pageProps}}) {
+function MyApp({Component, pageProps: {session, ...pageProps}}) {
   return (
     <>
       <GlobalStyles />
       <StyledContainer>
         <h1>🐟 Next Auth Demo 🐟</h1>
-        <Component {...pageProps} />
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
       </StyledContainer>
     </>
   );
